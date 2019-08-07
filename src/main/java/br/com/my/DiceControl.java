820a3d4050a7001d09e637797fa8a2d1d263409f
Alexandre Mattje <amattje@gmail.com>
2019-08-06 23:44:55 -0300

    some unit test

--- src/main/java/br/com/my/DiceControl.java
+++ src/main/java/br/com/my/DiceControl.java
@@ -56,74 +56,69 @@ public class DiceControl {
         this.rebuild();
     }
 
+    public void removeLast() {
+        this.elements.pop();
+        this.rebuild();
+    }
+
     private void rebuild() {
         Iterator<DiceElement> it = this.elements.iterator();
         StringBuilder fullDiceBuilder = new StringBuilder();
-        boolean hasOne = false;
-        boolean signalAdded = false;
-        boolean signal = false;
         DiceElementType previous = null;
         while (it.hasNext()) {
             DiceElement el = it.next();
 
             if (previous != null) {
                 switch (previous) {
-                case DICE:
-                    switch (el.type) {
                     case DICE:
+                        switch (el.type) {
+                            case DICE:
+                            case NUMBER:
+                                fullDiceBuilder.append("+");
+                                fullDiceBuilder.append(el.value);
+                                break;
+                            case SIGNAL:
+                                fullDiceBuilder.append(el.value);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
                     case NUMBER:
-                        fullDiceBuilder.append("+");
-                        fullDiceBuilder.append(el.value);
+                        switch (el.type) {
+                            case DICE:
+                            case SIGNAL:
+                                fullDiceBuilder.append(el.value);
+                                break;
+                            case NUMBER:
+                                fullDiceBuilder.append(el.value);
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     case SIGNAL:
-                        fullDiceBuilder.append(el.value);
+                        switch (el.type) {
+                            case DICE:
+                            case NUMBER:
+                                fullDiceBuilder.append(el.value);
+                                break;
+                            case SIGNAL:
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     default:
                         break;
-                    }
-                    break;
-                case NUMBER:
-                    switch (el.type) {
-                    case DICE:
-                        fullDiceBuilder.append(el.value);
-                        break;
-                    case NUMBER:
-                        fullDiceBuilder.append("+");
-                        fullDiceBuilder.append(el.value);
-                        break;
-                    case SIGNAL:
-                        fullDiceBuilder.append(el.value);
-                        break;
-                    default:
-                        break;
-                    }
-                    break;
-                case SIGNAL:
-                    switch (el.type) {
-                    case DICE:
-                        fullDiceBuilder.append(el.value);
-                    case NUMBER:
-                        fullDiceBuilder.append(el.value);
-                        break;
-                    case SIGNAL:
-                        break;
-                    default:
-                        break;
-                    }
-                    break;
-                default:
-                    break;
                 }
             } else {
                 fullDiceBuilder.append(el.value);
             }
-            System.out.println(el);
             previous = el.type;
-            hasOne = true;
         }
         this.fullDice = fullDiceBuilder.toString();
 
-        System.out.println(this.fullDice);
 
     }
 
